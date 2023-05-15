24067573a7ebd38ba28d8113a010eb01e1783fef
Breanna Mitchell <125221907+Bmitche24@users.noreply.github.com>
2023-05-15 16:17:29 -0400

    Updated crowdfunding_db_schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,37 +1,45 @@
 -- Create the Category table
-CREATE TABLE Category (
-  category_id VARCHAR PRIMARY KEY,
-  category VARCHAR
+CREATE TABLE category (
+    category_id varchar (100)   NOT NULL,
+    category varchar (100)  NOT NULL,
+	primary key (category_id)
 );
 
 -- Create the Subcategory table
-CREATE TABLE Subcategory (
-  subcategory_id VARCHAR PRIMARY KEY,
-  subcategory VARCHAR
+CREATE TABLE subcategory (
+    subcategory_id varchar (100)  NOT NULL,
+    subcategory varchar (100)  NOT NULL,
+	primary key (subcategory_id)
 );
 
 -- Create the Contacts table
-CREATE TABLE Contacts (
-  contact_id INT PRIMARY KEY,
-  first_name VARCHAR,
-  last_name VARCHAR,
-  email VARCHAR
+CREATE TABLE contacts (
+    contact_id int NOT NULL,
+    first_name varchar (100)  NOT NULL,
+    last_name varchar (100)  NOT NULL,
+    email varchar (100)  NOT NULL,
+	primary key (contact_id)
 );
 
+
 -- Create the Campaign table
-CREATE TABLE Campaign (
-  cf_id INT PRIMARY KEY,
-  contact_id INT REFERENCES Contacts(contact_id),
-  company_name VARCHAR,
-  description VARCHAR,
-  goal DOUBLE PRECISION,
-  pledged DOUBLE PRECISION,
-  outcome VARCHAR,
-  backers_count INT,
-  country VARCHAR,
-  currency VARCHAR,
-  launched_date DATE,
-  end_date DATE,
-  category_id VARCHAR REFERENCES Category(category_id),
-  subcategory_id VARCHAR REFERENCES Subcategory(subcategory_id)
+CREATE TABLE campaign (
+    cf_id int NOT NULL,
+    contact_id int NOT NULL,
+    company_name varchar (100)  NOT NULL,
+    description varchar (100)  NOT NULL,
+    goal double   NOT NULL,
+    pledged double   NOT NULL,
+    outcome varchar (100)  NOT NULL,
+    backers_count int   NOT NULL,
+    country varchar (100)  NOT NULL,
+    currency varchar (100)  NOT NULL,
+    launch_date date   NOT NULL,
+    end_date date   NOT NULL,
+    category_id varchar (100)  NOT NULL,
+    subcategory_id varchar (100)  NOT NULL,
+	primary key (cf_id),
+	foreign key (contact_id) references contacts(contact_id),
+	foreign key (category_id) references category(category_id),
+	foreign key (subcategory_id) references subcategory(subcategory_id)
 );
