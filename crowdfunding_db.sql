505eb750c314419c2b312aa1361ceb8d07d94fcd
Breanna Mitchell <125221907+Bmitche24@users.noreply.github.com>
2023-05-15 18:12:48 -0400

    added final sql & ERD

---
+++ crowdfunding_db.sql
@@ -0,0 +1,50 @@
+-- Create the Category table
+CREATE TABLE category (
+    category_id varchar (100)   NOT NULL,
+    category varchar (100)  NOT NULL,
+	primary key (category_id)
+);
+
+-- Create the Subcategory table
+CREATE TABLE subcategory (
+    subcategory_id varchar (100)  NOT NULL,
+    subcategory varchar (100)  NOT NULL,
+	primary key (subcategory_id)
+);
+
+-- Create the Contacts table
+CREATE TABLE contacts (
+    contact_id int NOT NULL,
+    first_name varchar (100)  NOT NULL,
+    last_name varchar (100)  NOT NULL,
+    email varchar (100)  NOT NULL,
+	primary key (contact_id)
+);
+
+
+-- Create the Campaign table
+CREATE TABLE campaign (
+    cf_id int NOT NULL,
+    contact_id int NOT NULL,
+    company_name varchar (100)  NOT NULL,
+    description varchar (100)  NOT NULL,
+    goal decimal NOT NULL,
+    pledged decimal NOT NULL,
+    outcome varchar (100)  NOT NULL,
+    backers_count int   NOT NULL,
+    country varchar (100)  NOT NULL,
+    currency varchar (100)  NOT NULL,
+    launch_date date   NOT NULL,
+    end_date date   NOT NULL,
+    category_id varchar (100)  NOT NULL,
+    subcategory_id varchar (100)  NOT NULL,
+	primary key (cf_id),
+	foreign key (contact_id) references contacts(contact_id),
+	foreign key (category_id) references category(category_id),
+	foreign key (subcategory_id) references subcategory(subcategory_id)
+);
+
+select * from contacts
+select * from category
+select * from subcategory
+select * from campaign
